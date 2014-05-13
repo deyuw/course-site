3f3005113f8484338fdf025c1e819cf65fb984a8
pencilz <dyw999@gmail.com>
2014-05-13 05:13:50 -0400

    transition to html 5 + a ton of other changes

---
+++ scripts/main.js
@@ -0,0 +1,17 @@
+var $line_left = $('#line_left');
+$(document).scroll(function() {
+  $line_left.css({display: $(this).scrollTop() > 20? "block":"none"});
+});
+var $line_right = $('#line_right');
+$(document).scroll(function() {
+  $line_right.css({display: $(this).scrollTop() > 20? "block":"none"});
+});
+
+var $line_left_long = $('#line_left_long');
+$(document).scroll(function() {
+  $line_left_long.css({display: $(this).scrollTop() > 20? "block":"none"});
+});
+var $line_right_long = $('#line_right_long');
+$(document).scroll(function() {
+  $line_right_long.css({display: $(this).scrollTop() > 20? "block":"none"});
+});
