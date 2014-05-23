6b57f73ea67d25bef77245c65c70c9c885009ce5
pencilz <dyw999@gmail.com>
2014-05-23 13:49:26 +0800

    intro js

---
+++ content.js
@@ -0,0 +1,17 @@
+document.write("hello");
+var content = document.getElementById("section");
+
+function 1(){
+	content.innerHTML='
+			<br>
+			<h1>Simple Induction</h1>
+      <p>In this video, I talk about simple induction, the first type of induction in the list of induction methods to be taught in this class. You can download the video by right clicking on the video.</p>
+      <iframe width="650" height="450" src="http://www.youtube.com/embed/0LtSgSsnu7o?rel=0" frameborder="0" allowfullscreen></iframe>
+      </br>
+      </br>
+      <p>This video talks about well-ordering.</p>
+      <iframe width="650" height="450" src="http://www.youtube.com/embed/DeMEQXoB7T4?rel=0" frameborder="0" allowfullscreen></iframe>';
+}
+function 2(){
+	content.innerHTML="yo";
+}
--- server.js
+++ server.js
@@ -1,3 +1,2 @@
 var connect = require('connect');
 connect().use(connect.static(__dirname)).listen(8080);
-// connect().use(connect.static("http://192.168.1.1")).listen(8080);
