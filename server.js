0abe6f4b5fd698f654ebf8626ba66c31e93a4612
Deyu Wang <dyw999@gmail.com>
2015-02-19 14:40:09 -0500

    Added server instructions

--- server.js
+++ server.js
@@ -1,3 +1,8 @@
 // used for testing purposes
 var connect = require('connect');
-connect().use(connect.static(__dirname)).listen(8080, '192.168.1.102');
+connect().use(connect.static(__dirname)).listen(8080, '192.168.1.123');
+
+// instruction
+// make sure network is has same ip address as one above
+// in terminal > node server.js
+// in browser: [ip address:8080/index.html]
